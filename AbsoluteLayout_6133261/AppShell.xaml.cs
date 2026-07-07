17fb839bb5d991fd3fd01aecdab482150c28ba63
Jefferson Arce <jeffersonarce439@gmail.com>
2026-07-07 10:26:41 -0600

    AbsoluteLayout MAUI

---
+++ AbsoluteLayout_6133261/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AbsoluteLayout_6133261
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AbsoluteLayout_6133261/StackLayout.xaml.cs
@@ -0,0 +1,9 @@
+namespace AbsoluteLayout_6133261;
+
+public partial class StackLayout : ContentPage
+{
+	public StackLayout()
+	{
+		InitializeComponent();
+	}
+}
