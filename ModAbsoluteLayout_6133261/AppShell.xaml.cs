86eaf4855d2c4b7c8d9d6d0a4ce60eaeb936a53a
Jefferson Arce <jeffersonarce439@gmail.com>
2026-07-13 09:58:47 -0600

    AbsoluteLayout Modificado

---
+++ ModAbsoluteLayout_6133261/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ModAbsoluteLayout_6133261
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ModAbsoluteLayout_6133261/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ModAbsoluteLayout_6133261
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
